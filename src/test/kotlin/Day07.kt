0a001af04f029ddefb94cfc0384ead15b942da7e
ricall <richard.m.allwood@gmail.com>
2024-12-08 10:29:22 +1000

    Cleaned up code + added more efficient concat operation

--- src/test/kotlin/Day07.kt
+++ src/test/kotlin/Day07.kt
@@ -5,46 +5,59 @@ import org.ricall.day07.Operator.*
 import java.io.File
 import kotlin.test.assertEquals
 
-data class Calibration(val result: Long, val values: List<Long>) {
-    fun apply(operator: Operator): Calibration {
-        val (v1, v2) = values.slice(0..1)
-        val newValues = mutableListOf(operator.apply(v1, v2))
-        newValues.addAll(values.slice(2..<values.size))
+data class Calibration(val target: Long, val values: List<Long>)
 
-        return Calibration(result = result, values = newValues)
-    }
-}
-
-enum class Operator(val apply: (Long, Long) -> Long) {
+enum class Operator(val evaluate: (Long, Long) -> Long) {
     ADD({ x, y -> x + y }),
     MULTIPLY({ x, y -> x * y }),
-    CONCAT({ x, y -> "${x}${y}".toLong()})
+    CONCAT({ x, y ->
+        var part = y
+        var multiplier = 1
+        while (part > 0) {
+            part /= 10
+            multiplier *= 10
+        }
+        x * multiplier + y
+    })
 }
 
 fun parseInput(text: String) = text.lines().filter(String::isNotBlank).map { line ->
-    val (result, valueText) = line.split(':')
+    val (target, valueText) = line.split(':')
     val values = valueText.trim().split(' ').map { it.toLong() }
 
-    Calibration(result = result.toLong(), values = values)
+    Calibration(target = target.toLong(), values = values)
 }
 
-fun isValidCalibration(calibration: Calibration) : Boolean {
-    if (calibration.values.size == 1) {
-        return calibration.result == calibration.values[0]
+fun checkCalibrationByRecursion(operators: List<Operator>, target: Long, current: Long, values: List<Long>): Boolean {
+    if (values.isEmpty()) {
+        return target == current
+    }
+    if (current > target) {
+        return false
     }
 
-    return isValidCalibration(calibration.apply(ADD)) || isValidCalibration(calibration.apply(MULTIPLY))
-}
+    return operators.any { operator ->
+        val newValues = values.toMutableList()
+        val value = newValues.removeFirst()
 
-fun isValidCalibrationWithAllOperators(calibration: Calibration) : Boolean {
-    if (calibration.values.size == 1) {
-        return calibration.result == calibration.values[0]
+        checkCalibrationByRecursion(operators, target, operator.evaluate(current, value), newValues)
     }
-
-    return isValidCalibrationWithAllOperators(calibration.apply(ADD))
-            || isValidCalibrationWithAllOperators(calibration.apply(MULTIPLY))
-            || isValidCalibrationWithAllOperators(calibration.apply(CONCAT))
 }
+
+fun checkCalibration(operators: List<Operator>, calibration: Calibration): Boolean {
+    val values = calibration.values.toMutableList()
+    val current = values.removeFirst()
+
+    return checkCalibrationByRecursion(operators, calibration.target, current, values)
+}
+
+fun sumOfValidCalibrationPartOne(text: String) = parseInput(text)
+    .filter { checkCalibration(listOf(ADD, MULTIPLY), it) }
+    .sumOf { it.target }
+
+fun sumOfValidCalibrationPartTwo(text: String) = parseInput(text)
+    .filter { checkCalibration(listOf(ADD, MULTIPLY, CONCAT), it) }
+    .sumOf { it.target }
 
 val TEST_DATA = """
     |190: 10 19
@@ -60,36 +73,28 @@ val TEST_DATA = """
 class Day07 {
     @Test
     fun `part 1 test data`() {
-        val result = parseInput(TEST_DATA)
-            .filter(::isValidCalibration)
-            .sumOf { it.result }
+        val result = sumOfValidCalibrationPartOne(TEST_DATA)
 
         assertEquals(3749, result)
     }
 
     @Test
     fun `part 1`() {
-        val result = parseInput(File("./inputs/day7.txt").readText())
-            .filter(::isValidCalibration)
-            .sumOf { it.result }
+        val result = sumOfValidCalibrationPartOne(File("./inputs/day7.txt").readText())
 
         assertEquals(12940396350192, result)
     }
 
     @Test
     fun `part 2 test data`() {
-        val result = parseInput(TEST_DATA)
-            .filter(::isValidCalibrationWithAllOperators)
-            .sumOf { it.result }
+        val result = sumOfValidCalibrationPartTwo(TEST_DATA)
 
         assertEquals(11387, result)
     }
 
     @Test
     fun `part 2`() {
-        val result = parseInput(File("./inputs/day7.txt").readText())
-            .filter(::isValidCalibrationWithAllOperators)
-            .sumOf { it.result }
+        val result = sumOfValidCalibrationPartTwo(File("./inputs/day7.txt").readText())
 
         assertEquals(106016735664498, result)
     }
