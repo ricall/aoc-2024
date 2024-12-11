b0a0c34d77446c8b9d248a82be7ba2b046f0d169
ricall <richard.m.allwood@gmail.com>
2024-12-11 10:19:56 +1000

    Simplified answer

--- src/test/kotlin/Day10.kt
+++ src/test/kotlin/Day10.kt
@@ -15,15 +15,15 @@ private val TEST_DATA = """
     |01329801
     |10456732""".trimMargin()
 
-private data class Point(val x: Int, val y: Int) {
-    operator fun plus(direction: Direction) = Point(x + direction.delta.x, y + direction.delta.y)
-}
-
 private enum class Direction(val delta: Point) {
     NORTH(Point(0, -1)),
     EAST(Point(1, 0)),
     SOUTH(Point(0, 1)),
     WEST(Point(-1, 0)),
+}
+
+private data class Point(val x: Int, val y: Int) {
+    operator fun plus(direction: Direction) = Point(x + direction.delta.x, y + direction.delta.y)
 }
 
 private class Topography(input: String) {
@@ -33,7 +33,7 @@ private class Topography(input: String) {
     val height: Int
 
     init {
-        trailheads = mutableListOf<Point>()
+        trailheads = mutableListOf()
         map = input.lines().mapIndexed { y, line ->
             line.mapIndexed { x, c ->
                 val height = c.digitToInt()
@@ -47,67 +47,53 @@ private class Topography(input: String) {
         height = map.size
     }
 
-    private fun isValid(p: Point) = p.x in 0..<width && p.y in 0..<height
-
-    private fun uniqueEndpoints(start: Point): Set<Point> {
-        val height = map[start.y][start.x]
-        if (height == 9) {
-            return setOf(start)
-        }
-        return sequenceOf(NORTH, SOUTH, EAST, WEST)
-            .map { start + it }
-            .filter(::isValid)
-            .filter { map[it.y][it.x] == height + 1 }
-            .flatMap(::uniqueEndpoints)
-            .toSet()
-    }
-    fun countUniqueEndpoints(start: Point) = uniqueEndpoints(start).size
-
-    fun countPaths(start: Point): Int {
-        val height = map[start.y][start.x]
-        if (height == 9) {
-            return 1
-        }
-        return sequenceOf(NORTH, SOUTH, EAST, WEST)
-            .map { start + it }
-            .filter(::isValid)
-            .filter { map[it.y][it.x] == height + 1 }
-            .map(::countPaths)
-            .sum()
+    private fun heightAt(p: Point) = when {
+        p.x in 0..<width && p.y in 0..<height -> map[p.y][p.x]
+        else -> Integer.MAX_VALUE
     }
 
-    fun sumTrailheadScores(algorithm: (Point) -> Int) = trailheads.map(algorithm).sum()
+    private fun trailEndpoints(point: Point): List<Point> {
+        val height = heightAt(point)
+        if (height == 9) {
+            return listOf(point)
+        }
+        return sequenceOf(NORTH, SOUTH, EAST, WEST)
+            .map { direction -> point + direction }
+            .filter { heightAt(it) == height + 1 }
+            .flatMap(::trailEndpoints)
+            .toList()
+    }
+
+    fun sumTrailheadScores(): Int = trailheads.map(::trailEndpoints).sumOf { it.distinct().size }
+
+    fun sumTrailheadRatings(): Int = trailheads.map(::trailEndpoints).sumOf { it.size }
 }
 
 class Day10 {
     @Test
     fun `part 1 test data`() {
-        val model = Topography(TEST_DATA)
-        val result = model.sumTrailheadScores(model::countUniqueEndpoints)
+        val result = Topography(TEST_DATA).sumTrailheadScores()
 
         assertEquals(36, result)
     }
 
     @Test
     fun `part 1`() {
-        val model = Topography(File("./inputs/day10.txt").readText())
-        val result = model.sumTrailheadScores(model::countUniqueEndpoints)
+        val result = Topography(File("./inputs/day10.txt").readText()).sumTrailheadScores()
 
         assertEquals(796, result)
     }
 
     @Test
     fun `part 2 test data`() {
-        val model = Topography(TEST_DATA)
-        val result = model.sumTrailheadScores(model::countPaths)
+        val result = Topography(TEST_DATA).sumTrailheadRatings()
 
         assertEquals(81, result)
     }
 
     @Test
     fun `part 2`() {
-        val model = Topography(File("./inputs/day10.txt").readText())
-        val result = model.sumTrailheadScores(model::countPaths)
+        val result = Topography(File("./inputs/day10.txt").readText()).sumTrailheadRatings()
 
         assertEquals(1942, result)
     }
