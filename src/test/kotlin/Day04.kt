1cdf6b05e7f9d863b312403b45d02b9f9d8e4c2b
ricall <richard.m.allwood@gmail.com>
2024-12-18 14:46:59 +1000

    Cleaned up code

--- src/test/kotlin/Day04.kt
+++ src/test/kotlin/Day04.kt
@@ -1,8 +1,97 @@
+package org.ricall.day04
+
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
+import org.ricall.day04.Direction.*
 
-private val TEST_DATA = """
+private data class Point(val y: Int, val x: Int) {
+    operator fun plus(other: Point) = Point(y + other.y, x + other.x)
+    operator fun plus(direction: Direction) = Point(y + direction.vector.y, x + direction.vector.x)
+}
+
+private enum class Direction(val vector: Point) {
+    NW(Point(-1, -1)),
+    N(Point(-1, 0)),
+    NE(Point(-1, 1)),
+    E(Point(0, 1)),
+    SE(Point(1, 1)),
+    S(Point(1, 0)),
+    SW(Point(1, -1)),
+    W(Point(0, -1)),
+}
+
+private class Day04Search(text: String) {
+
+    // ---------------- PART ONE
+
+    private fun getXMASCount(position: Point) = Direction.entries
+        .map { direction -> textAt(position, direction, 4)}
+        .count { text -> text == "XMAS" }
+
+    private fun countXMASAt(position: Point) = when(charAt(position)) {
+        'X' -> getXMASCount(position)
+        else -> 0
+    }
+
+    fun countXMAS() = totalFor(::countXMASAt)
+
+    // ----------------- PART TWO
+
+    private fun isMASCross(position: Point): Boolean {
+        return listOf(
+            textAt(position + NW, SE, 3),
+            textAt(position + SW, NE, 3),
+        ).all { word -> word == "MAS" || word == "SAM" }
+    }
+
+    private fun getMASCrossCountAt(position: Point) = when (isMASCross(position)) {
+        true -> 1
+        else -> 0
+    }
+
+    private fun smartCountMASCrossAt(position: Point) = when (charAt(position)) {
+        'A' -> getMASCrossCountAt(position)
+        else -> 0
+    }
+
+    fun countMASCrosses() = totalFor(::smartCountMASCrossAt)
+
+    // ----------------- HELPERS
+
+    private val lines = text.lines()
+
+    private fun charAt(position: Point): Char {
+        val (y, x) = position
+        if (y < 0 || y >= lines.size || x < 0 || x >= lines[y].length) {
+            return ' '
+        }
+        return lines[y][x]
+    }
+
+    private fun textAt(position: Point, direction: Direction, length: Int): String {
+        var currentPoint = position
+
+        return (0..<length).map {
+            val ch = charAt(currentPoint)
+            currentPoint += direction
+            ch
+        }.joinToString(separator = "")
+    }
+
+    private inline fun totalFor(totalAt: (Point) -> Int): Int {
+        var total = 0
+        for (y in lines.indices) {
+            for (x in lines[y].indices) {
+                total += totalAt(Point(y, x))
+            }
+        }
+        return total
+    }
+}
+
+class Day04 {
+    private val TEST_DATA = """
     |MMMSXXMASM
     |MSAMXMSMSA
     |AMXSXMAAMM
@@ -14,7 +103,6 @@ private val TEST_DATA = """
     |MAMMMXMMMM
     |MXMXAXMASX""".trimMargin()
 
-class Day04 {
     @Test
     fun `part 1 test data`() {
         val result = Day04Search(TEST_DATA).countXMAS()
