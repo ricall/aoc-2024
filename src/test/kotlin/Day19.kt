a5e2c099252cc25b10c56ce2432068c3d38fda3c
ricall <richard.m.allwood@gmail.com>
2024-12-19 16:50:08 +1000

    Finished day 19

---
+++ src/test/kotlin/Day19.kt
@@ -0,0 +1,81 @@
+package org.ricall.day19
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+private class LinenLayout(input: String) {
+    private val patterns: List<String>
+    private val designs: List<String>
+    private val cache = mutableMapOf<String, Long>()
+
+    init {
+        val parts = input.split("\n\n")
+        patterns = parts[0].split(", ")
+        designs = parts[1].lines()
+    }
+
+    private fun countLinenArrangements(steps: List<String>, design: String, count: Long = 0): Long =
+        cache.getOrElse(design) {
+            if (design.isEmpty()) {
+                return 1
+            }
+            var arrangements = count
+            for (step in steps) {
+                if (design.startsWith(step)) {
+                    arrangements += countLinenArrangements(steps, design.substring(step.length))
+                }
+            }
+            cache[design] = arrangements // getOrPut doesn't play nicely with recursion
+            return arrangements
+        }
+
+    private fun numberOfArrangementsForDesigns(): List<Long> =
+        designs.map { design -> countLinenArrangements(patterns, design) }
+
+    fun validDesignPatternCount() = numberOfArrangementsForDesigns().filter { it > 0 }.count()
+
+    fun totalTowelArrangements() = numberOfArrangementsForDesigns().sum()
+}
+
+class Day19 {
+    private val TEST_DATA = """
+        |r, wr, b, g, bwu, rb, gb, br
+        |
+        |brwrr
+        |bggr
+        |gbbr
+        |rrbgbr
+        |ubwu
+        |bwurrg
+        |brgr
+        |bbrgwb""".trimMargin()
+
+    @Test
+    fun `part 1 test data`() {
+        val result = LinenLayout(TEST_DATA).validDesignPatternCount()
+
+        assertEquals(6, result)
+    }
+
+    @Test
+    fun `part 1`() {
+        val result = LinenLayout(File("./inputs/day19.txt").readText()).validDesignPatternCount()
+
+        assertEquals(298, result)
+    }
+
+    @Test
+    fun `part 2 test data`() {
+        val result = LinenLayout(TEST_DATA).totalTowelArrangements()
+
+        assertEquals(16, result)
+    }
+
+    @Test
+    fun `part 2`() {
+        val result = LinenLayout(File("./inputs/day19.txt").readText()).totalTowelArrangements()
+
+        assertEquals(572248688842069, result)
+    }
+}
